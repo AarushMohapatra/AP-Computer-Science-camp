53f86ba6860449033aa7e4ca1206653000bb283a
AarushMohapatra <aarushmohapatra11@gmail.com>
2026-09-25 15:52:53 -0400

    finish file

--- src/FullYearCourse/MyFirstPrograms.java
+++ src/FullYearCourse/MyFirstPrograms.java
@@ -5,12 +5,25 @@ import java.util.Scanner;
 public class MyFirstPrograms {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("-----Program1-----");
-        Program1();
-        System.out.println("------------------\n");
-        System.out.println("-----Program2-----");
-        Program2();
-        System.out.println("------------------\n");
+        System.out.println("Which program would you like to run? (1-4)");
+        int choice = sc.nextInt();
+        sc.nextLine();
+        switch (choice) {
+            case 1:
+                Program1();
+                break;
+            case 2:
+                Program2();
+                break;
+            case 3:
+                Program3();
+                break;
+            case 4:
+                Program4();
+                break;
+            default:
+                System.out.println("Invalid choice.");
+        }
     }
     public static void Program1() {
         System.out.print("Enter your name: ");
@@ -24,5 +37,24 @@ public class MyFirstPrograms {
             names[i] = sc.nextLine();
         }
         System.out.println(names[2] + ", " + names[1] + ", " + names[0]);
+    }
+    public static void Program3() {
+        System.out.print("Enter a weight in pounds: ");
+        double weight = sc.nextDouble();
+        System.out.println("Your weight in mercury is: " + weight*0.4);
+        System.out.println("Your weight in venus is: " + weight*0.9);
+        System.out.println("Your weight in mars is: " + weight*0.38);
+        System.out.println("Your weight in jupiter is: " + weight*2.3);
+        System.out.println("Your weight in saturn is: " + weight*1.1);
+        System.out.println("Your weight in uranus is: " + weight*0.92);
+        System.out.println("Your weight in neptune is: " + weight*1.2);
+    }
+    public static void Program4() {
+        System.out.print("Give me any amount of seconds: ");
+        int seconds = sc.nextInt();
+        int hours = seconds / 3600;
+        int minutes = (seconds % 3600) / 60;
+        int remainingSeconds = seconds % 60;
+        System.out.println(hours+", "+minutes+", "+remainingSeconds+".");
     }
 }
